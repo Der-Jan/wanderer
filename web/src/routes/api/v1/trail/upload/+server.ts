be9cde7ac3d1150d49073c64501a170f9f26333b
Jan Riechers <de@r-jan.de>
2026-01-24 13:31:44 +0000

    Privacy is null for new profiles

--- web/src/routes/api/v1/trail/upload/+server.ts
+++ web/src/routes/api/v1/trail/upload/+server.ts
@@ -47,7 +47,8 @@ export async function PUT(event: RequestEvent) {
             trail.location ??= location;
         }
 
-        trail.public = event.locals.settings.privacy.trails == "public"
+
+        trail.public = event.locals.settings.privacy?.trails == "public"
 
         // const log = new SummitLog(trail.date as string, {
         //     distance: trail.distance,
